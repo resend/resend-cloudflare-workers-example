9f530877563097152da45152c0053423d988df99
Mark Ross <6730333+krazkidd@users.noreply.github.com>
2024-12-31 22:19:50 -0700

    Get email info from request and do basic validation. Add configurable email recipient.

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,41 @@ import { EmailTemplate } from './emails/email-template';
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if (request.method !== "POST") {
+			return new Response(null, { status: 403, statusText: 'Method not allowed' });
+		}
+
+		const formData = await request.formData();
+
+		const name = formData.get('name');
+		const email = formData.get('email');
+		const subject = formData.get('subject');
+		const message = formData.get('message');
+
+		if (!name) {
+			return new Response(null, { status: 403, statusText: 'Name must be provided' });
+		} else if (!email) {
+			return new Response(null, { status: 403, statusText: 'Email must be provided' });
+		} else if (!subject) {
+			return new Response(null, { status: 403, statusText: 'Subject must be provided' });
+		} else if (!message) {
+			return new Response(null, { status: 403, statusText: 'Message must be provided' });
+		}
+
 		const resend = new Resend(env.RESEND_API_KEY);
 
 		const data = await resend.emails.send({
-			from: 'Acme <onboarding@resend.dev>',
-			to: ['delivered@resend.dev'],
-			subject: 'hello world',
-			react: <EmailTemplate firstName="John" />,
+			from: `${name} <${email}>`,
+			to: [env.RESEND_RECIPIENT],
+			subject,
+			react: <EmailTemplate firstName={name} />,
+		});
+
+		console.log({
+			name,
+			email,
+			subject,
+			message
 		});
 
 		return new Response(JSON.stringify(data), {
