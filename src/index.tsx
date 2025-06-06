b6e84a203100c144f1c9286adf9d754d1d2051e8
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2025-06-06 15:38:00 +0000

    feat: lint (#29)

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,24 @@ import { Resend } from 'resend';
 import { EmailTemplate } from './emails/email-template';
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const resend = new Resend('re_123456789'/* env.RESEND_API_KEY */);
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    const resend = new Resend('re_123456789' /* env.RESEND_API_KEY */);
 
-		const data = await resend.emails.send({
-			from: 'Acme <onboarding@resend.dev>',
-			to: ['delivered@resend.dev'],
-			subject: 'hello world',
-			react: <EmailTemplate firstName="John" />,
-		});
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'hello world',
+      react: <EmailTemplate firstName="John" />,
+    });
 
-		return new Response(JSON.stringify(data), {
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	},
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  },
 };
